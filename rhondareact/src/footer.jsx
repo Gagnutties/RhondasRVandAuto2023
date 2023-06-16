94de4c64755880bcc756572a49ee71252e54cfe2
Gagnutties <m3gam1ll1ona1r@gmail.com>
2023-06-16 12:49:34 -0400

    Added footer element

--- rhondareact/src/footer.jsx
+++ rhondareact/src/footer.jsx
@@ -2,8 +2,16 @@ export default function Footer() {
     return (
         <footer id="footer_section">
             <nav  id="backtotop">
-                <a href="#root">Back to Top</a>
+                <br></br>
+                <a id="homebuttonfooter"href="#root">Back to Top</a>
             </nav>
+            <h3>Rhonda's RV and Auto is a BUY HERE PAY HERE 
+            car lot.</h3>
+            <h3>Address: </h3> <p>1251 State Road 256 Austin Indiana 47102</p>
+            <h3>Office Phone Number: </h3><p>812-794-2222</p>
+            <h3>Email: </h3><p>rhondagsmith2@gmail.com</p>
+            <h3>Hours: </h3><p>8:00pm to 5:00pm Monday to Friday. 8:00pm to 3:00pm Saturday</p>
+            <div id="bottomspace"></div>
         </footer>
     );
 }
