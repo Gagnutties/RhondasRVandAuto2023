6ba8162449c668058a654af2c1636e4b044fd948
Gagnutties <m3gam1ll1ona1r@gmail.com>
2024-08-17 13:42:53 -0400

    caars

--- rhondareact/src/exportvehicle.jsx
+++ rhondareact/src/exportvehicle.jsx
@@ -342,11 +342,10 @@ let VolkswagonCC_3467 = {
     allvehiclearray.push(VolkswagonCC_3467, ChevyEquinox_1867, ChevyMailbu_5434
                         ,Mazda3_8461, BuickVerano_1173, HyundaiAzera_2729
                         ,GMCCanyon_2352, HondaAccord_0201
-                        ,FordFiesta_2502,FordEscape_5486,FordFocus_0808
-                        ,ChevyImpala_4300,KiaSedona_7505,HondaCivic_8477,DodgeRam_6820
-                        ,FordF150_5255,HyundaiSonata_2394
-                        ,HondaAccord_2915, ScionXBox_0000
-                        ,HyundaiGenesis_9388, HyundaiSantaFe_0000
+                        ,FordFiesta_2502,FordEscape_5486
+                        ,ChevyImpala_4300,HondaCivic_8477,DodgeRam_6820
+                        ,HyundaiSonata_2394
+                        ,HondaAccord_2915
                         ,HyundaiSonata_0000, LincolnTownCar_2099, ToyotaCamry_9058
                         ,ToyotaCorolla_0720, ToyotaCamry_1974
                         ,ToyotaSienna_3806, KiaSoul_1957,);
